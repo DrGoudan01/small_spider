652e0781e9d2043df0f1f95aae348742b580b835
DrGoudan01 <18813290224@163.com>
2017-10-30 15:30:42 +0800

    test

--- test/html_test.py
+++ test/html_test.py
@@ -1,4 +1,4 @@
-#__author__ = 'li'
+#__author__ = 'li111'
 # -*- coding:utf-8 -*-
 import requests
 import re
@@ -10,4 +10,11 @@ course = soup.find_all('div',{'class':'col-md-4','class':'col-sm-6','class':'cou
 #定义查找的div标签，这个方法是BeautifulSoup定义的
 #遍历这个经过解析又经过筛选的列表
 for i in course:
-    title = i.find('div',{'class':'course-name'})
+    title = i.find('div',{'class':'course-name'}).get_text()
+    study_people = i.find('span',{'class':'course-per-num','class':'pull-left'}).get_text()
+    study_people = re.sub("\D","",study_people)
+    try:
+        tag = i.find('find',{'class':'course-per-num','class':'pull-right'}).get_text()
+    except:
+        tag = "课程"#对于没有课程标签的将标签设置为课程
+    print ("{}    学习人数：{}   {}".format(tag,study_people,title))
